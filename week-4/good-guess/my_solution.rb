e78fe18b45edc91f65d5ea1ce4a8a260f93105dd
jonathanNicolas <contact.jonathan.nicolas@gmail.com>
2016-02-13 11:16:36 +0800

    Add good-guess

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def good_guess?(arg)
+  if arg == 42
+    return true
+  else
+    return false
+  end
+end
+
