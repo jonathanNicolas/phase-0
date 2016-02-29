91980f6911eecaa7381b8e423bed6abd4fbac0b1
jonathanNicolas <contact.jonathan.nicolas@gmail.com>
2016-02-29 17:22:16 +0800

    Made changes

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,46 @@
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+# release1
+# What are these methods doing? Initializes age,name,occupation. reads them then changes them.
+# How are they modifying or returning the value of instance variables? there are two methods change_my and what_is, the first one passes a new argument and assigns it to an instance variable. The second one returns the instance variable.
+
+# release2
+# What changed between the last release and this release?The what_is_age method was altered with an attr reader method.
+# What was replaced? what_is_age methog
+# Is this code simpler than the last?yes
+
+# release3
+# What changed between the last release and this release?The change_my_age got deleted
+# What was replaced?attr_writer : age
+# Is this code simpler than the last?yes
+
+
+
+# Reflection
+# What is a reader method? a method that returns instance variable
+# What is a writer method? a method that writes instance variable
+# What do the attr methods do for you?methods are already defined with attr no need to start from scratch
+# Should you always use an accessor to cover your bases? Why or why not?No. Sometimes you want reader or writer specifically
+# What is confusing to you about these methods?
