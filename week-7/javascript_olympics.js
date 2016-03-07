c6dcd8cc603f69bd09d1b464dd77d1db74c6caa8
jonathanNicolas <contact.jonathan.nicolas@gmail.com>
2016-03-07 15:32:06 +0800

    Made changes

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -86,34 +86,57 @@
 //  console.log(even_only(integers))
 
 
-function filter(array,test) {
-  var passed = [];
-  for (var i = 0; i < array.length; i++) {
-    if(test(array[i]))
-      passed.push(array[i]);
-  }
+// First, we set up the source and destination arrays
+var integers = [3,8,1,6,2,0,5,7];
+var odds     = [];
+var evens    = [];
 
-  return passed;
+// Then define the loop
+for (var n = 0; n < integers.length; n++) {
+ if (integers[n] % 2 === 0) {
+   evens.push(integers[n]);
+ } else {
+   odds.push(integers[n]);
+ }
 }
 
-// var integers = [3,8,1,6,2,0,5,7];
+console.log(integers);
+console.log(odds);
+console.log(evens);
 
-console.log(filter([3,8,1,6,2,0,5,7],function(arg){
-  true ;
-}));
+
+// function filter(array,test) {
+//   var passed = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if(test(array[i]))
+//       passed.push(array[i]);
+//   }
+
+//   return passed;
+// }
+
+// // var integers = [3,8,1,6,2,0,5,7];
+
+// console.log(filter([3,8,1,6,2,0,5,7],function(arg){
+//   true ;
+// }));
 
 
 // "We built this city"
 
 
-// function Athlete(name,age,sport,quote){
-//   this.name = name;
-//   this.age = age;
-//   this.sport = sport;
-//   this.quote = quote;
-// }
-// var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
-// console.log(michaelPhelps.constructor === Athlete)
-// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+function Athlete(name,age,sport,quote){
+  this.name = name;
+  this.age = age;
+  this.sport = sport;
+  this.quote = quote;
+}
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 // Reflection
+
+// What JavaScript knowledge did you solidify in this challenge? I learned to work with .reverse and .split. I attempted to use a funtion Filter for the even number problem but wasn't able to work it out just yet still it was an interesting attempt.
+// What are constructor functions? Constructor function is a function to create and Object type in JS, then you create the instances by using .New method
+// How are constructors different from Ruby classes (in your research)? In JavaScript any function can become a constructor function unlike in Ruby , I would say that is the biggest difference.
